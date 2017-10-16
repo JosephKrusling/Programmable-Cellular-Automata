d1f661a31a786165fdf886679ac8338f16019635
Tanner Bornemann <bornemtr@mail.uc.edu>
2017-10-16 15:56:54 -0400

    adding basic tank functions

--- game/entity/Tank.js
+++ game/entity/Tank.js
@@ -2,6 +2,16 @@ function Tank(id, x, y) {
     this.id = id;
     this.x = x;
     this.y = y;
+    this.health = 10; // setting default health to 10 arbitrarily [said tanner on Mon, Oct 16, 2017 at T15:52:16 -5:00]
+}
+
+Tank.prototype.getHealth = function (){
+    return this.health;
+};
+
+// this tank gets hit and reduces health by 1
+Tank.prototype.getRekt = function (){
+    --this.health;
 }
 
 
