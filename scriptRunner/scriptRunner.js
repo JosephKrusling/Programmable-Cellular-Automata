03658f90f2a3c4e1f73a6d43992cd7468988ba73
Joseph Krusling <joseph@krusling.net>
2017-10-18 19:34:01 -0400

    Improved performance of script runner

--- scriptRunner/scriptRunner.js
+++ scriptRunner/scriptRunner.js
@@ -1,4 +1,4 @@
-const {VM} = require('vm2');
+const {VM, VMScript} = require('vm2');
 const fs = require('fs');
 const socket = require('socket.io-client')('http://localhost:3000');
 
@@ -10,11 +10,13 @@ socket.on('connect', () => {
 
 let scriptSource = fs.readFileSync('./example.js', {encoding: 'utf8'});
 
+const script = new VMScript(scriptSource);
+
 socket.on('tick', (arg) => {
     // console.log(JSON.stringify(arg));
 
     let vm = new VM({
-        timeout: 100,
+        timeout: 50,
         sandbox: {
             state: arg.state,
             print: function(text) {
@@ -27,7 +29,7 @@ socket.on('tick', (arg) => {
     try {
         let packet = {};
         let startTime = process.hrtime();
-        let scriptResult = vm.run(scriptSource);
+        let scriptResult = vm.run(script);
 
         packet.executionTime = process.hrtime(startTime)[1]/1000000; // milliseconds
         packet.desiredMove = scriptResult;
